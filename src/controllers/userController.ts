3a59aec91917062f6859be7ecaaae86b412aec43
joaopaulo <joaopaulosg17@gmail.com>
2022-03-31 10:38:09 -0300

    CRUD basico terminado

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -12,12 +12,55 @@ class UserController{
                 password:password,
                 email:email
             });
-            res.status(201).json(`${name} cadastrado com sucesso`)
+            res.status(201).json(`${name} cadastrado com sucesso`);
         }catch(error){
-            console.log(error)
-            res.status(500).json(`${error}`)
+            console.log(error);
+            res.status(500).json(error);
         }
-        
+    }
+
+    async getUsers(req:Request,res:Response){
+        try{
+            const response = await myDataSource.manager.find(User);
+            res.status(200).json(response);
+        }catch(error){
+            res.status(500).json(error);
+        }
+    }
+
+    async updateUser(req:Request,res:Response){
+        const id = parseInt(req.params.id);
+        const {name,username,password,email} = req.body;
+        try{
+            const user = await myDataSource.manager.findOneBy(User,{id:id});
+            if(user){
+                user.name = name;
+                user.username = username;
+                user.password = password;
+                user.email = email;
+                await myDataSource.manager.save(user)
+                res.status(200).json(`User atualizado`);
+            }else{
+                res.status(500).json(`User não existe`);
+            }
+        }catch(error){
+            res.status(500).json(error);
+        }
+    }
+    
+    async deleteUser(req:Request,res:Response){
+        const id = parseInt(req.params.id);
+        try{
+            const user = await myDataSource.manager.findOneBy(User,{id:id});
+            if(user){
+                await myDataSource.manager.remove(user)
+                res.status(200).json(`User deletado`);
+            }else{
+                res.status(500).json(`User não existe`);
+            }
+        }catch(error){
+            res.status(500).json(error);
+        }
     }
 }
 
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,4 +5,10 @@ const user = new UserController()
 
 userRouter.post('/add',user.newUser)
 
+userRouter.get('/',user.getUsers)
+
+userRouter.put('/update/:id',user.updateUser)
+
+userRouter.delete('/delete/:id',user.deleteUser)
+
 export default userRouter;
